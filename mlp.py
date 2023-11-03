b8ac27c31fc1521f70d689c0090220a7d66d8ca1
Delctor <89162148+Delctor@users.noreply.github.com>
2023-11-02 19:25:22 -0500

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -1,13 +1,23 @@
 
 import numpy as np
 from layer import *
+from numba.experimental import jitclass
+import numba as nb
 
+spec = [
+        ("nInputs", nb.uint64), 
+        ("nOutputs", nb.uint64), 
+        ("layers", nb.types.ListType(Layer.class_type.instance_type)), 
+        ]
+
+
+@jitclass(spec)
 class NeuralNetwork:
     def __init__(self, nInputs, layers, activationFunctions):
         self.nInputs = nInputs
         self.nOutputs = layers[-1]
         size = [nInputs] + layers
-        self.layers = [Layer(size[i], size[i + 1], activationFunctions[i]) for i in range(len(layers))]
+        self.layers = nb.typed.List([Layer(size[i], size[i + 1], activationFunctions[i]) for i in range(len(layers))])#[Layer(size[i], size[i + 1], activationFunctions[i]) for i in range(len(layers))]
         
     def fit(self, X, Y, learningRate, epochs, bathSize, printLoss = False):
         X = np.expand_dims(X, 1)
@@ -22,20 +32,22 @@ class NeuralNetwork:
                     a = layer.a
                 
                 self.layers[-1].dz = (-2 * (Y[i] - a)) * self.layers[-1].da if self.layers[-1].activationFunctionType != "softmax" else np.dot((-2 * (Y[i] - a)), self.layers[-1].da)
-                nextLayer = None
+                self.layers[-1].backwardLastLayer()
+                nextLayer = self.layers[-1]
                 # Backward
-                for layer in reversed(self.layers):
-                    layer.backward(nextLayer)
-                    nextLayer = layer
-
+                for j in range(len(self.layers) - 2, -1, -1):
+                    self.layers[j].backward(nextLayer)
+                    nextLayer = self.layers[j]
+                
                 if ((i + 1) % bathSize) == 0:
+                    
                     for layer in self.layers:
                         layer.updateParameters(bathSize, learningRate)
                 
-                loss += (Y[i] - self.layers[-1].a) ** 2
+                loss += ((Y[i] - self.layers[-1].a) ** 2).mean()
             loss /= len(X)
             if printLoss:
-                print(loss.mean())
+                print(loss)
             
     def predict(self, X):
         X = np.expand_dims(X, 1)
@@ -48,9 +60,3 @@ class NeuralNetwork:
                 a = layer.a
             YHat[i] = a
         return YHat
-        
-        
-        
-        
-        
-        
