1ec39a4527bd0171d0b801c1151ec9b721f914ce
Delctor <89162148+Delctor@users.noreply.github.com>
2023-11-02 19:25:01 -0500

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,11 +1,40 @@
 
 import numpy as np
 from activationFunctions import *
+from numba.experimental import jitclass
+import numba as nb
 
+spec = [
+        ("weights", nb.float64[:, :]), 
+        ("biases", nb.float64[:, :]), 
+        ("dwAcumulator", nb.float64[:, :]), 
+        ("dbAcumulator", nb.float64[:, :]), 
+        ("x", nb.float64[:, :]), 
+        ("a", nb.float64[:, :]), 
+        ("da", nb.float64[:, :]), 
+        ("dz", nb.float64[:, :]), 
+        ("activationFunction", nb.float64[:, :](nb.float64[:, :], nb.float64[:, :], nb.b1).as_type()), 
+        ("activationFunctionType", nb.types.unicode_type)
+        ]
+
+
+@jitclass(spec)
 class Layer:
     def __init__(self, nInputs, nNeurons, activationFunction):
-        exec("self.activationFunction = " + activationFunction)
+        
         self.activationFunctionType = activationFunction
+        
+        if activationFunction == "sigmoid":
+            self.activationFunction = sigmoid
+        elif activationFunction == "tanh":
+            self.activationFunction = tanh
+        elif activationFunction == "relu":
+            self.activationFunction = relu
+        elif activationFunction == "lrelu":
+            self.activationFunction = lrelu
+        elif activationFunction == "softmax":
+            self.activationFunction = softmax
+        
         self.weights = np.random.rand(nInputs, nNeurons)
         self.biases = np.random.rand(1, nNeurons)
         
@@ -15,16 +44,19 @@ class Layer:
     def forward(self, x):
         self.x = x
         z = np.dot(x, self.weights) + self.biases
-        self.a = self.activationFunction(None, z, False)
+        self.a = self.activationFunction(z, z, False)
         self.da = self.activationFunction(self.a, z, True)
     
     def forwardPredict(self, x):
         z = np.dot(x, self.weights) + self.biases
-        return self.activationFunction(None, z, False)
+        return self.activationFunction(z, z, False)
     
-    def backward(self, nextLayer = None):
-        if nextLayer is not None:
-            self.dz = self.da * np.dot(nextLayer.dz, nextLayer.weights.T)
+    def backwardLastLayer(self):
+        self.dwAcumulator += np.dot(self.x.T, self.dz)
+        self.dbAcumulator += self.dz
+    
+    def backward(self, nextLayer):
+        self.dz = self.da * np.dot(nextLayer.dz, nextLayer.weights.T)
         
         self.dwAcumulator += np.dot(self.x.T, self.dz)
         self.dbAcumulator += self.dz
